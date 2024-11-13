74667eba0063270263acd55bcb406af19d96c3dc
Omar Aguayo <126422987+AdventureFi@users.noreply.github.com>
2024-11-12 19:57:38 -0600

    Add files via upload

--- src/vocalconsonante/VocalConsonante.java
+++ src/vocalconsonante/VocalConsonante.java
@@ -3,13 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package vocalconsonante;
+
+import java.util.Scanner;
+
 public class VocalConsonante {
     public static void main(String[] args) {
-        char ch = args[0].charAt(0);
-
+        Scanner sc =new Scanner(System.in);
+        String letra;
+        char ch;
+        do{
+        letra= sc.next();
+        ch=letra.charAt(0);
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " es una vocal.");
         else 
             System.out.println(ch + " es una consonante.");
+        }while(ch!='q');
     }
 }
